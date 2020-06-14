b860b05517942d050739185c8f635177f01a8f18
pre-ska <wolfwar@outlook.com>
2020-06-14 19:41:51 +0200

    data import

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const Tour = require('../../models/tourModel');
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/tourModel");
+const Review = require("../../models/reviewModel");
+const User = require("../../models/userModel");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
@@ -15,20 +17,28 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(con => {
-    console.log('connected to database');
+  .then((con) => {
+    console.log("connected to database");
   });
 
 //read json file with tours examples
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+//read json file with tours examples
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+//read json file with tours examples
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 //import data to database
 const importData = async () => {
   try {
     await Tour.create(tours);
-    console.log('data successfully loaded');
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log("data successfully loaded");
   } catch (error) {
     console.log(error);
   }
@@ -39,16 +49,18 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    console.log('data successfully deleted');
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log("data successfully deleted");
   } catch (error) {
     console.log(error);
   }
   process.exit();
 };
 
-if (process.argv[2] === '--import') {
+if (process.argv[2] === "--import") {
   importData();
-} else if (process.argv[2] === '--delete') {
+} else if (process.argv[2] === "--delete") {
   deleteData();
 }
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,52 +1,52 @@
 //10-2
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 //10-12
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'User name is required'],
+    required: [true, "User name is required"],
     trim: true,
-    maxlength: [40, 'A user name must have less than 30 characters'],
-    minlength: [3, 'A user name must have minimum 3 characters']
+    maxlength: [40, "A user name must have less than 30 characters"],
+    minlength: [3, "A user name must have minimum 3 characters"],
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    required: [true, "Email is required"],
     unique: true,
     lowercase: true,
     trim: true,
-    validate: [validator.isEmail, 'Pleas provide valid email']
+    validate: [validator.isEmail, "Pleas provide valid email"],
   },
   photo: String,
   role: {
     // 10-11 implemntiram user roles za admine
     type: String,
-    enum: ['user', 'guide', 'lead-guide', 'admin'], // vrste korsinika po ovlastima
-    default: 'user'
+    enum: ["user", "guide", "lead-guide", "admin"], // vrste korsinika po ovlastima
+    default: "user",
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
+    required: [true, "Password is required"],
     trim: true,
     minlength: 8,
-    select: false //ne prikazuje se nigdje...npr u odgovoru RESPONSE prema klijentu
+    select: false, //ne prikazuje se nigdje...npr u odgovoru RESPONSE prema klijentu
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'please confirm your password'],
+    required: [true, "please confirm your password"],
     trim: true,
     validate: {
       //10-4 usporedi 2 polja, radi samo na CREATE i SAVE!! (a ne npr findOneAndUpdate)
-      validator: function(el) {
+      validator: function (el) {
         return el === this.password;
       },
-      message: 'Passwords are not the same'
-    }
+      message: "Passwords are not the same",
+    },
   },
   passwordChangedAt: Date, //10-9 zbog kontrole passworda dali je promjenjen
   passwordResetToken: String,
@@ -55,14 +55,14 @@ const userSchema = new mongoose.Schema({
     // dali je korisniko racun aktivan ili ne
     type: Boolean,
     default: true,
-    select: false // ovo znaci da ovaj prop ACTIVE nece biti dostupan klijentskoj strani na uvid kada vratim user dokument, zivi samo u DB
-  }
+    select: false, // ovo znaci da ovaj prop ACTIVE nece biti dostupan klijentskoj strani na uvid kada vratim user dokument, zivi samo u DB
+  },
 });
 
 //10-4 hashing password bcrypt - PRE-HOOK
-userSchema.pre('save', async function(next) {
+userSchema.pre("save", async function (next) {
   // ako password nije modificiran, zovi next()... "this" je dokument
-  if (!this.isModified('password')) return next();
+  if (!this.isModified("password")) return next();
 
   // hasiraj password sa "cost" 12
   this.password = await bcrypt.hash(this.password, 12); // 10 je default
@@ -75,8 +75,8 @@ userSchema.pre('save', async function(next) {
 //10-14 pre-hook za provjeru promjene passworda prilikom snimanja
 // ako priolikom snimanja dokumenta password NIJE promjenjen ili AKO JE OVO NOVI dokument (koji isto zapise password)
 // return next() --- jednostavno iskoci iz fumnkcije
-userSchema.pre('save', function(next) {
-  if (!this.isModified('password') || this.isNew) return next();
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
 
   // a ako prodje, onda zapisi novi property passwordChangedAt u user dokument
   this.passwordChangedAt = Date.now();
@@ -89,7 +89,7 @@ userSchema.pre('save', function(next) {
 // query je FIND
 // korsitim regular expression tako da mi sve sto pocinje sa find koristi ovaj middleware
 // find, findById...
-userSchema.pre(/^find/, function(next) {
+userSchema.pre(/^find/, function (next) {
   // THIS = current query - trenutna pretraga koja ce se nadopuniti
   // znaci prije rezultata svake pretrage ovaj middleware jos dodatno filtrira po ACTIVE
   this.find({ active: { $ne: false } }); //not equal to false - jer ako stavim samo active: true...onda dokumenti koji nemau uopce polje active nece biti dohvaceni
@@ -99,7 +99,7 @@ userSchema.pre(/^find/, function(next) {
 
 //10-7 static instance method - ovo ce biti dostupno na svim dokumentima neke kolekcije...znaci mogu ovu metodu pozvati na bilo kojem USer modelu u npr authControleru
 //hasira password prilikom logina...onda taj hash usporedjujem sa hashom u DB...
-userSchema.methods.correctPassword = async function(
+userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
 ) {
@@ -108,7 +108,7 @@ userSchema.methods.correctPassword = async function(
 
 //10-9 static instance method - ovo ce biti dostupno na svim dokumentima neke kolekcije...znaci mogu ovu metodu pozvati na bilo kojem Ueer dokumentu u npr authControleru
 //ova metoda provjerava dali je promjenjen password od korisnika u vremenu od kada je izdan token -JWTTimestamp
-userSchema.methods.changePasswordAfter = function(JWTTimestamp) {
+userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
   //this je trenutni dokument
   // ako je korisnik promjenio password, nesto ce biti zapisano - timestamp...ako nije, nece postojati
   if (this.passwordChangedAt) {
@@ -126,15 +126,15 @@ userSchema.methods.changePasswordAfter = function(JWTTimestamp) {
 
 //10-12 static instance method - ovo ce biti dostupno na svim dokumentima neke kolekcije...znaci mogu ovu metodu pozvati na bilo kojem Ueer dokumentu u npr authControleru
 //ova metoda ce kreirati random token ako korisnik zeli da promjeni password
-userSchema.methods.createPasswordResetToken = function() {
+userSchema.methods.createPasswordResetToken = function () {
   // build in node module - crypto kreira token
-  const resetToken = crypto.randomBytes(32).toString('hex');
+  const resetToken = crypto.randomBytes(32).toString("hex");
 
   // - NIKAD NE SPREMAM U DB CISTI TOKEN...vec ga hashiram sa crypto metodom createHash
   this.passwordResetToken = crypto
-    .createHash('sha256')
+    .createHash("sha256")
     .update(resetToken)
-    .digest('hex');
+    .digest("hex");
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
@@ -142,7 +142,7 @@ userSchema.methods.createPasswordResetToken = function() {
 };
 
 /******************************************** */
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 /******************************************** */
 
 module.exports = User;
