6cf8db52321043408caf884590ebdbd00b384fa3
pre-ska <wolfwar@outlook.com>
2020-06-22 15:33:35 +0200

    stripe fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -36,7 +36,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: "usd",
         quantity: 1,
@@ -71,7 +73,7 @@ const createBookingCheckout = async (session) => {
   const tourId = session.client_reference_id;
   const userId = session.customer_id;
   // const user = (await User.findOne({email: session.custorem_email})).id
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tourId, userId, price });
 };
@@ -91,7 +93,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${error}`);
   }
 
-  if (event.type === "checkout.session.complete")
+  if (event.type === "checkout.session.completed")
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
