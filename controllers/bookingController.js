41ebbd29f51113ac915f7c2cb8441f38f3e32515
pre-ska <wolfwar@outlook.com>
2020-06-22 16:07:22 +0200

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -69,9 +69,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tourId = session.client_reference_id;
+  const tour = session.client_reference_id;
   // const userId = session.customer_id;
-  const userId = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
