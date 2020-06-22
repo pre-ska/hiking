1d3bce4c98cd3655b9525703273e1619eaa06c92
pre-ska <wolfwar@outlook.com>
2020-06-22 15:51:12 +0200

    stripe fix 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -30,7 +30,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     success_url: `${req.protocol}://${req.get("host")}/my-tours`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    customer_id: req.user.id,
     client_reference_id: req.params.tourId,
     line_items: [
       {
